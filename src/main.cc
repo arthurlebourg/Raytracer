b337b35c69e98391de31f679e14df0f69c957bab
Martin Chevereau <martin.chevereau@epita.fr>
2022-02-25 18:50:03 +0100

    changed main.cc 's architecture to have diffused color computation

--- src/main.cc
+++ src/main.cc
@@ -9,13 +9,27 @@
 #include "uniform_texture.hh"
 #include "vector3.hh"
 
+Color diffused_color(std::shared_ptr<Object> object, Scene scene,
+                     Vector3 hit_point)
+{
+    scene = scene;
+    auto material = object->get_texture(hit_point);
+    return material.get_color();
+    // TODO :add proper diffusion computation
+}
+
 int main()
 {
     double fov_w = 110;
-    double fov_h = 90;
+    double fov_h = 110;
     double dist_to_screen = 50.0;
-    Camera cam = Camera(Vector3(0, 0, 0), Vector3(0, 0, 1), Vector3(0, 1, 0),
-                        fov_w / 2, fov_h / 2, dist_to_screen);
+
+    Vector3 camCenter(0, 0, 0);
+    Vector3 camFocus(0, 0, 1);
+    Vector3 camUp(0, 1, 0);
+
+    Camera cam = Camera(camCenter, camFocus, camUp, fov_w / 2, fov_h / 2,
+                        dist_to_screen);
     Scene sc = Scene(cam);
 
     Uniform_Texture green_tex = Uniform_Texture(Material(Color(0, 255, 0), 1));
@@ -30,6 +44,7 @@ int main()
 
     double img_width = 680;
     double img_height = 460;
+    Color default_color(0, 0, 0);
 
     Image img = Image("bite.ppm", img_width, img_height);
 
@@ -39,10 +54,12 @@ int main()
         {
             Ray ray = cam.get_ray(x / img_width, y / img_height);
             float min_dist = std::numeric_limits<float>::max();
-            Material mat = Material(Color(0, 0, 0), 0);
+            std::shared_ptr<Object> object = nullptr;
+            std::optional<Vector3> hit = std::nullopt;
+            // finds objects with closest point
             for (size_t i = 0; i < sc.objects_.size(); i++)
             {
-                std::optional<Vector3> hit = sc.objects_[i]->hit(ray);
+                hit = sc.objects_[i]->hit(ray);
                 if (hit.has_value())
                 {
                     float new_dist =
@@ -50,11 +67,19 @@ int main()
                     if (new_dist < min_dist)
                     {
                         min_dist = new_dist;
-                        mat = sc.objects_[i]->get_texture(hit.value());
+                        object = sc.objects_[i];
                     }
                 }
             }
-            img.set(mat.get_color(), x, y);
+
+            if (object == nullptr)
+            {
+                img.set(default_color, x, y);
+            }
+            else
+            {
+                img.set(diffused_color(object, sc, hit.value()), x, y);
+            }
         }
     }
     img.save();

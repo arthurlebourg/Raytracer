46af3dcc59fc21131b7e6bc6405dc9b6b1dffee0
Arthur Le Bourg <lebourgarthur@gmail.com>
2022-03-03 11:59:11 +0100

    first push

---
+++ include/blob.hh
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <optional>
+
+#include "ray.hh"
+#include "texture_material.hh"
+
+class Blob
+{
+public:
+    Blob(Vector3 pos, double e, double d, float seuil_potentiel)
+
+        float potentiel(Vector3 pos)
+
+            private:
+};
